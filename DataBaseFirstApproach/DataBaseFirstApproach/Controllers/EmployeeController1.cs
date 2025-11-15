76201fbe9c35c5ac09a25a7825660ed61a1f4c32
TaimoorQadri123 <dchota374@gmail.com>
2025-11-15 10:52:43 +0500

    done

---
+++ DataBaseFirstApproach/DataBaseFirstApproach/Controllers/EmployeeController1.cs
@@ -0,0 +1,67 @@
+﻿using DataBaseFirstApproach.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace DataBaseFirstApproach.Controllers
+{
+    public class EmployeeController1 : Controller
+    {
+        private readonly EmployeeDbContext _context;
+
+        public EmployeeController1(EmployeeDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<IActionResult> Index()
+        {
+           var emp = await _context.Employees.ToListAsync();
+            return View(emp);
+        }
+
+        [HttpGet]
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+
+        public async Task<IActionResult> Create(Employee emp)
+        {
+            await _context.Employees.AddAsync(emp);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var emp =  await _context.Employees.FirstOrDefaultAsync(x => x.EmpId == id );
+            return View(emp);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Employee emp)
+        {
+              _context.Employees.Update(emp);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var emp = await _context.Employees.FirstOrDefaultAsync(x => x.EmpId == id);
+            return View(emp);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Delete(Employee emp)
+        {
+            _context.Employees.Remove(emp);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ DataBaseFirstApproach/DataBaseFirstApproach/Program.cs
@@ -0,0 +1,36 @@
+using DataBaseFirstApproach.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataBaseFirstApproach
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            builder.Services.AddDbContext<EmployeeDbContext>(x => x.UseSqlServer(builder.Configuration.GetConnectionString("Connect")));
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Employee}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
