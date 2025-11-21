1902b2369ed996dc46ee9a556ce320356e4494f3
TaimoorQadri123 <dchota374@gmail.com>
2025-11-22 04:42:59 +0500

    done class 14

---
+++ ModelHandling/ModelHandling/ModelHandling/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ModelHandling.Models;
+
+namespace ModelHandling.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Students>  Students { get; set; }
+
+        public DbSet<Category> Category { get; set; }   
+
+        public DbSet<Products> Products { get; set; }   
+
+    }
+}
